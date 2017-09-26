0c890b371566d5cc0d2dd6f68bb23fd7221fa8ea
alekseik1 <alekseik1@yandex.ru>
2017-09-26 15:14:32 +0300

    Инициализация 1 и 2

--- 3sem/labs/week4-/solutions/matrix_mult.cpp
+++ 3sem/labs/week4-/solutions/matrix_mult.cpp
@@ -10,66 +10,38 @@ using namespace std;
 int main() {
     int M, N, K;
     cin >> M >> N >> K;
-    double** buffer = (double**) malloc(M*N*sizeof(double*));
+    double* buffer = (double*) malloc(M*N*sizeof(double));
     if(buffer == NULL) {
         // Не удалось создать, уходим
         return -1;
     }
+    // Первый инициализируем
     for(int i = 0; i < M; i++) {
-        *(buffer + i*sizeof(double*)) = (double*) malloc(N*sizeof(double));
-    }
-
-
-    int** buffer1 = (int**) calloc(N*K, sizeof(int**));
-    if(buffer1 == NULL) {
-        // Не удалось создать, уходим
-        return -1;
-    }
-    for(int i = 0; i < N; i++) {
-        *(buffer1 + i*sizeof(int*)) = (int*) calloc(K, sizeof(int));
-    }
-
-    double* buffer2 = new double[M];
-
-    // Initialize first matrix
-    for(int i=0; i < M; i++) {
         for(int j=0; j < N; j++) {
-            // ОЧЕНЬ СЛОЖНО
-            // берем buffer, сдвигаем его на i позиций (по памяти это будет сдвиг на i размеров указателя
-            // второго порядка **), затем разыменовываем его, получаем указатель первого порядка. Его мы сдвигаем на
-            // j позиций (опять же, прибавление j размеров указателя первого порядка) и полученное место памяти
-            // разыменовываем - получаем наше значение массива. Я ненавижу C++
-            *(*(buffer + i*sizeof(double**)) + j*sizeof(double*)) = i + j;
+            *(buffer + (i*N+j)*sizeof(double)) = (double) (i+j);
         }
     }
-    // cout first matrix
+
     for(int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            cout <<*(*(buffer + i*sizeof(double**)) + j*sizeof(double*))  << " ";
+        for(int j=0; j < N; j++) {
+            cout << *(buffer + (i*N+j)*sizeof(double)) << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
 
-    // Initialize second matrix
-    // Поехали
+    int* buffer1 = (int*) calloc(K*N, sizeof(int));
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < K; j++) {
-            *(*(buffer1 + i*sizeof(int**)) + j*sizeof(int*)) = (i == j) ? 1 : 0;
-            // Тут то же самое, только я юзнул пафосный тернарный оператор
+            *(buffer1 + (i*K+j)*sizeof(int)) = ((i == j) ? 1 : 0);
         }
     }
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < K; j++) {
-            cout << *(*(buffer1 + i*sizeof(int**)) + j*sizeof(int*)) << " ";
-            // Вывожу
+            cout << *(buffer1 + (i*K+j)*sizeof(int)) << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
-
     free(buffer);
     free(buffer1);
-    buffer1 = NULL;
-    buffer = NULL;
-
     return 0;
 }
