92e9dce1724f29dce43f8bf6a47b0bab6a1df82e
alekseik1 <alekseik1@yandex.ru>
2017-10-03 14:09:39 +0300

    B is done

---
+++ 3sem/contests/week5-/solutions/B.cpp
@@ -0,0 +1,17 @@
+//
+// Created by aleksei on 03.10.17.
+//
+
+#include <iostream>
+
+int main() {
+    std::string s = "";
+    std::getline(std::cin, s);
+    int res = 1;
+    for(int i = s.size()-1; i >= 0; i--) {
+        if(s[i] == ' ') {
+            res++;
+        }
+    }
+    std::cout << res << "\n";
+}
