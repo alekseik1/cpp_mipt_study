546d804cddeaa8fd4d3c1260f5d589258a725857
alekseik1 <alekseik1@yandex.ru>
2017-10-17 15:20:45 +0300

    F is done

--- 3sem/contests/week7-/solutions/F.cpp
+++ 3sem/contests/week7-/solutions/F.cpp
@@ -1,4 +1,36 @@
 //
 // Created by aleksei on 17.10.17.
 //
+#include <iostream>
 
+using namespace std;
+
+int Search_Binary (int arr[], int left, int right, int key)
+{
+    int midd = 0;
+    while (1)
+    {
+        midd = (left + right) / 2;
+
+        if (key < arr[midd])
+            right = midd - 1;
+        else if (key > arr[midd])
+            left = midd + 1;
+        else
+            return midd;
+
+        if (left > right)
+            return -1;
+    }
+}
+
+int main() {
+    int N, x;
+    cin >> N >> x;
+    int k[N];
+    for(int i = 0; i < N; i++) {
+        cin >> k[i];
+    }
+    int pos = Search_Binary(k, 0, N, x);
+    cout << ((pos == -1) ? -1 : pos + 1);
+}
