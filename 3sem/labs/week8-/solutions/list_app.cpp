3e0c709bd6414ca41dfd8a05d461891622231c4b
alekseik1 <alekseik1@yandex.ru>
2017-10-24 13:10:49 +0300

    Цикл по добавлению 90-100 в конец списка

--- 3sem/labs/week8-/solutions/list_app.cpp
+++ 3sem/labs/week8-/solutions/list_app.cpp
@@ -8,6 +8,9 @@ int main() {
     for(int i = 0; i < 10; ++i) {
         list_insert(intList, i);
     }
+    for(int i = 90; i < 100; ++i) {
+        list_insert_back(intList, i);
+    }
     list_print(intList, std::cout);
     std::cout << std::endl;
     list_destroy(intList);
