9eb60a58da82b6e84f4d25bf28863b934ed9753d
alekseik1 <alekseik1@yandex.ru>
2017-10-24 13:56:57 +0300

    list_insert -- метод для добавления элемента в любую позицию

--- 3sem/labs/week8-/solutions/linked_list.hpp
+++ 3sem/labs/week8-/solutions/linked_list.hpp
@@ -65,6 +65,32 @@ void list_insert_back(List<DataType> &lst, const DataType &value)
 }
 
 template<typename DataType>
+void list_insert(List<DataType>& l, const DataType &value,  int pos)
+{
+    if(pos == 0)  // Вставляем в начало, если pos == 0
+    {
+        list_insert(l, value);
+        return;
+    } else if(pos == l.size) // Вставляем в конец, если pos == l.size (я нумерую ноды листа с нуля и этого вам советую
+    {
+        list_insert_back(l, value);
+        return;
+    }
+
+    tNode<DataType> *node = l.begin;    // Сначала на старте
+    for(int i = 0; i < pos-1; i++) {
+        node = node->next;  // POS раз идем дальше
+    }
+    // Вот мы и на месте. Теперь надо ПОСЛЕ node поставить наш новый элемент
+    // Сделаем хитро: создадим новую ноду, сделаем ей next - следующий после node элемент, а самому node в качестве следующего запишем созданный newNode
+    // Не понятно? Представьте все это в голове и станет ясно. Я не зря в цикле дошел до pos-1, а не до pos
+    tNode<DataType> *newNode = new tNode<DataType>;
+    newNode->data = value;
+    newNode->next = node->next;
+    node->next = newNode;
+}
+
+template<typename DataType>
 void list_print(const List<DataType> &lst, std::ostream &out)
 {
     tNode<DataType> *p = lst.begin;
