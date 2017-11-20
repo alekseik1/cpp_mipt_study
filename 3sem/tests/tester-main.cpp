4e9505ba1e7b0ac20b3de3125e699e94499c2c63
alekseik1 <alekseik1@yandex.ru>
2017-11-20 17:01:50 +0300

    Unit тесты на ранней стадии

---
+++ 3sem/tests/tester-main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by aleksei on 19.11.17.
+//
+
+#ifndef TESTER_MAIN
+#define TESTER_MAIN
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "week11-test.hpp"
+
+#endif
---
+++ 3sem/tests/week11-test.hpp
@@ -0,0 +1,14 @@
+//
+// Created by aleksei on 19.11.17.
+//
+
+#ifndef WEEK11_WEEK11_TEST_HPP
+#define WEEK11_WEEK11_TEST_HPP
+
+#include "catch.hpp"
+
+TEST_CASE("Week 11", "[11][week11]") {
+
+}
+
+#endif //WEEK11_WEEK11_TEST_HPP
