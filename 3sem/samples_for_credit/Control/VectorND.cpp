739c8f072a9d4d38a8be273dad37cd309d37b64f
alekseik1 <alekseik1@yandex.ru>
2017-12-10 14:43:17 +0300

    Fixed double construction calls. PASSES TESTS

--- 3sem/samples_for_credit/Control/VectorND.cpp
+++ 3sem/samples_for_credit/Control/VectorND.cpp
@@ -4,34 +4,8 @@
 
 #include "VectorND.hpp"
 
-VectorND::VectorND(int n, std::initializer_list<double> coords) {
-    _N = n;
-
-    if(coords.size() == 0) {    // С пустым init_list мы заполним нулями
-        for(int i = 0; i < n; i++) {
-            _X[i] = 0;
-        }
-        return;
-    }
-
-    if(coords.size() != n) {
-        throw("Not enough coords to unpack!");
-    }
-    unsigned i = 0;
-    // TODO: Пользователь может скормить не double в init_list. Надо сделать на это проверку
-    for(auto& coord : coords) {
-        _X[i] = coord;
-        i++;
-    }
-}
-
-VectorND::VectorND(int n): VectorND(n, {})
+VectorND::VectorND(int n): PointND(n)
 {}
 
-VectorND::~VectorND() {
-
-}
-
-int VectorND::getDim() const noexcept {
-    return _N;
-}
+VectorND::VectorND(int n, std::initializer_list<double> coords): PointND(n, coords)
+{}
--- 3sem/samples_for_credit/Control/VectorND.hpp
+++ 3sem/samples_for_credit/Control/VectorND.hpp
@@ -13,14 +13,12 @@ class VectorND: public PointND {
 public:
     VectorND(int n, std::initializer_list<double> coords);
     explicit VectorND(int n);
-    virtual ~VectorND();
     VectorND operator+(const VectorND& other);
     VectorND operator-(const VectorND& other);
     VectorND operator-();
     double operator*(const VectorND& other);
     friend VectorND operator*(double k, const VectorND& v);
     friend VectorND operator*(const VectorND& v, double k);
-    int getDim() const noexcept;
 };
 
 
