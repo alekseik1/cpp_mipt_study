37d030f896e250ba2d0df807955c5a6c6c04624e
alekseik1 <alekseik1@yandex.ru>
2017-12-10 13:23:37 +0300

    ASSERT_DOUBLE instead of ASSERT_FLOAT

--- 3sem/samples_for_credit/Control/point-tests.cpp
+++ 3sem/samples_for_credit/Control/point-tests.cpp
@@ -10,16 +10,16 @@ TEST(PointND, PointND_Empty_Create) {
     PointND p;
 
     for(int i = 0; i < p.getDim(); ++i) {
-        ASSERT_FLOAT_EQ(p.get(i), 0.0);
+        ASSERT_DOUBLE_EQ(p.get(i), 0.0);
     }
 }
 
 TEST(PointND, PointND_Create_With_Params_TestParams) {
     PointND p1(3, {1.0, 2.0, 3.0});
 
-    ASSERT_FLOAT_EQ(p1.get(0), 1.0);
-    ASSERT_FLOAT_EQ(p1.get(1), 2.0);
-    ASSERT_FLOAT_EQ(p1.get(2), 3.0);
+    ASSERT_DOUBLE_EQ(p1.get(0), 1.0);
+    ASSERT_DOUBLE_EQ(p1.get(1), 2.0);
+    ASSERT_DOUBLE_EQ(p1.get(2), 3.0);
 }
 
 TEST(PointND, PointND_Params_Size_Mismatch) {
