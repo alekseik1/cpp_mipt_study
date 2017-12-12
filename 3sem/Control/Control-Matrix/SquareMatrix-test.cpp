db050844c1fcc0aa2478a8dae2039480f488f014
alekseik1 <alekseik1@yandex.ru>
2017-12-12 11:39:50 +0300

    Tests for getter, setter and size()

--- 3sem/Control/Control-Matrix/SquareMatrix-test.cpp
+++ 3sem/Control/Control-Matrix/SquareMatrix-test.cpp
@@ -5,6 +5,28 @@
 #include<gtest/gtest.h>
 #include "SquareMatrix.hpp"
 
-TEST(Matrix, EmptyCreation) {
-    Matrix<int> m();
+TEST(Matrix, SimpleCreation) {
+    Matrix<char> m(5);
+}
+
+TEST(Matrix, getSize) {
+    for(int i = 1; i < 5; i++) {
+        Matrix<double> m(i);
+        ASSERT_EQ(m.size(), i);
+    }
+}
+
+TEST(Matrix, SettersAndGetters) {
+    Matrix<int> m(5);
+    for(int i = 0; i < m.size(); i++) {
+        for(int j = 0; j < m.size(); j++) {
+            m.set(i, j, i+j);
+        }
+    }
+    // TEST
+    for(int i = 0; i < m.size(); i++) {
+        for(int j = 0; j < m.size(); j++) {
+            ASSERT_EQ(m.get(i, j), i+j);
+        }
+    }
 }
